e45daee389e3a47803ce424d77a7ccf611427c2e
ASUS <kjuneja_be24@thapar.edu>
2025-02-08 17:04:34 +0530

    all files arranged

--- src/components/UseContact.jsx
+++ src/components/UseContact.jsx
@@ -4,49 +4,42 @@ export default function UseContact() {
     const people = [
         {
             id: 1,
-            name: "John Doe",
-            designation: "Software Engineer",
+            name: "Ganeev Singh",
+            designation: "CEO",
             image:
-                "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+                "https://i.ibb.co/rfzyZSKP/Whats-App-Image-2025-01-22-at-12-58-40-668f6e3f.jpg",
         },
         {
             id: 2,
-            name: "Robert Johnson",
-            designation: "Product Manager",
+            name: "Aditya Gupta",
+            designation: "CTO",
             image:
-                "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+                "https://i.ibb.co/3YRs9jwh/Whats-App-Image-2025-01-22-at-13-00-50-be70da65.jpg",
         },
         {
             id: 3,
-            name: "Jane Smith",
-            designation: "Data Scientist",
+            name: "Keshav Juneja",
+            designation: "Software Developer",
             image:
-                "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+                "https://i.ibb.co/jPny69Nm/Whats-App-Image-2025-01-22-at-13-04-21-38f31d02.jpg",
         },
         {
             id: 4,
-            name: "Emily Davis",
+            name: "Shaksam Mishra",
             designation: "UX Designer",
             image:
-                "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+                "https://i.ibb.co/vxM9rghS/Whats-App-Image-2025-01-22-at-13-04-41-28ca3846.jpg",
         },
         {
             id: 5,
-            name: "Tyler Durden",
-            designation: "Soap Developer",
+            name: "Shivansh Shrivashtav",
+            designation: "CFO",
             image:
-                "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
-        },
-        {
-            id: 6,
-            name: "Dora",
-            designation: "The Explorer",
-            image:
-                "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+                "https://i.ibb.co/MytYSBbY/Whats-App-Image-2025-01-22-at-13-19-08-400cbd5f.jpg",
         },
     ];
     return (
-        <div className="flex justify-center items-center p-16 scale-150">
+        <div className="flex justify-center items-center p-32 scale-[2]">
             <AnimatedTooltip items={people} />
         </div>
     );
