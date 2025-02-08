d8efb820cc874eb477514abb9df821c43783dcb2
ASUS <kjuneja_be24@thapar.edu>
2025-02-08 15:54:49 +0530

    all files arranged

--- src/components/backend.jsx
+++ src/components/backend.jsx
@@ -3,7 +3,7 @@ import ApiConnect from "./ApiConnect.jsx";
 export default function Backend() {
     return (
         <>
-            <ApiConnect />
+            <ApiConnect/>
         </>
     )
 }
