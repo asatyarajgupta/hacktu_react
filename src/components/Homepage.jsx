7a797893def98dbe14629991f3dc5bef3ed81958
ASUS <kjuneja_be24@thapar.edu>
2025-02-08 17:38:55 +0530

    all files arranged

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -30,59 +30,59 @@ export default function Homepage() {
         {
             title: "Editrix AI",
             link: "https://editrix.ai",
-            thumbnail: "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+            thumbnail: "https://i.ibb.co/23Q01vsM/juniornigga.jpg",
         },
         {
             title: "Pixel Perfect",
             link: "https://app.pixelperfect.quest",
-            thumbnail: "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+            thumbnail: "https://i.ibb.co/FLndTbdj/exam.jpg",
         },
 
         {
             title: "Algochurn",
             link: "https://algochurn.com",
-            thumbnail: "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+            thumbnail: "https://i.ibb.co/d0ZFmXh6/senior.jpg",
         },
         {
             title: "Aceternity UI",
             link: "https://ui.aceternity.com",
-            thumbnail: "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+            thumbnail: "https://i.ibb.co/PsvJNpYh/foundation.jpg",
         },
         {
             title: "Tailwind Master Kit",
             link: "https://tailwindmasterkit.com",
-            thumbnail: "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+            thumbnail: "https://i.ibb.co/23Q01vsM/juniornigga.jpg",
         },
         {
             title: "SmartBridge",
             link: "https://smartbridgetech.com",
-            thumbnail: "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+            thumbnail: "https://i.ibb.co/FLndTbdj/exam.jpg",
         },
         {
             title: "Renderwork Studio",
             link: "https://renderwork.studio",
-            thumbnail: "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+            thumbnail: "https://i.ibb.co/d0ZFmXh6/senior.jpg",
         },
 
         {
             title: "Creme Digital",
             link: "https://cremedigital.com",
-            thumbnail: "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+            thumbnail: "https://i.ibb.co/PsvJNpYh/foundation.jpg",
         },
         {
             title: "Golden Bells Academy",
             link: "https://goldenbellsacademy.com",
-            thumbnail: "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+            thumbnail: "https://i.ibb.co/23Q01vsM/juniornigga.jpg",
         },
         {
             title: "Invoker Labs",
             link: "https://invoker.lol",
-            thumbnail: "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+            thumbnail: "https://i.ibb.co/FLndTbdj/exam.jpg",
         },
         {
             title: "E Free Invoice",
             link: "https://efreeinvoice.com",
-            thumbnail: "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+            thumbnail: "https://i.ibb.co/d0ZFmXh6/senior.jpg",
         },
     ]
     const items = [
